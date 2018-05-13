f7102891ec3b101b1484fe85b66908d075ef9912
chloesoe <info@chloesoe.ch>
2018-05-13 14:30:55 +0200

    improvements

--- cloud-backup.sh
+++ cloud-backup.sh
@@ -4,28 +4,29 @@
 #dirs without a slash at the end
 sourcedir=/home/dhw
 backupdir=/media/dhw/hetzner
-excludefile=/home/dhw/bin/cloud-exclude.txt
+excludefile=/home/dhw/backup/cloud-exclude.txt
 label=dhw
-ssh_identiyfile=/home/dhw/.ssh/hetzner_u159931 
+ssh_identiyfile=/home/dhw/.ssh/cloud_key
+#storage url file has to look like "user@example.com"
+ssh_storageurl=`cat /home/dhw/.ssh/storage_url`
 
 #mount backup direcorty with sshfs
-sshfs u159931@u159931.your-storagebox.de:/ $backupdir -o IdentityFile=$ssh_identiyfile -o idmap=user -o uid=$(id -u) -o gid=$(id -g)
-
+sshfs $ssh_storageurl:/ $backupdir -o IdentityFile=$ssh_identiyfile -o idmap=user -o uid=$(id -u) -o gid=$(id -g)
 
 cd $sourcedir
 
+#int_handler makes sure, the destination is unmounted
 int_handler()
 {
     echo "Interrupted."
     # Kill the parent process of the script.
     kill $PPID
+    fusermount -u $backupdir
     exit 1
 }
 trap 'int_handler' INT
 
 
-#test if backupdir is mounted 
-#if test -d $backupdir; then
 if mount | grep $backupdir > /dev/null; then
     rsync -avzk --delete --log-file=/home/dhw/backup/cloud-backup_$(date +'%Y%m%d').log --exclude-from=/$excludefile "$sourcedir/" "$backupdir/$label"
 else
